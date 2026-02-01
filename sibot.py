34173a61b466bee541f05fceb874d6ca2a120eac
dartmew <dartmew@yandex.com>
2026-02-01 17:26:02 +0300

    publish photo to Telegram channel.

--- sibot.py
+++ sibot.py
@@ -1,22 +1,32 @@
-import asyncio
 from telegram import Bot
 from dotenv import load_dotenv
 import os
 
 
-async def send_message(bot, chat_id):
-    await bot.send_message(chat_id=chat_id, text='Hello, greetings!', parse_mode='Markdown')
-    print('Message sent')
+def send_picture():
+    image_path = 'images/50291306061_2f9e350a85_o.jpg'
 
 
-async def main():
+    with open(image_path, 'rb') as photo_file:
+        bot.send_photo(chat_id=chat_id, photo=photo_file, caption='Классное фото - космос покоряет 🚀')
+
+def make_image_pull(images_folder):
+    images_files = []
+
+    for file in os.listdir(images_folder):
+        if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+            images_files.append(file)
+
+    return images_files
+
+def main():
     load_dotenv()
     token = os.environ['TG_BOT_TOKEN']
     chat_id = os.environ['TG_CHAT_ID']
-
     bot = Bot(token=token)
-    await send_message(bot, chat_id)
+    images_folder = 'images'
 
+    print(make_image_pull(images_folder))
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
